49da2c459304e69c927a3863b55174911de9ea43
Asael Tobar <2023158061@ub.edu.bz>
2025-04-18 12:31:51 -0600

    third commit

--- main.go
+++ main.go
@@ -5,18 +5,18 @@ import (
 	"net"
 )
 
-func handleConnection(conn net.Conn) {
+func handleConnection(conn net.Conn) { // Function to handle connections
 	defer conn.Close()
-	buf := make([]byte, 1024)
+	buf := make([]byte, 1024) // Can handle up to 1024 bytes
 
-	for {
-		n, err := conn.Read((buf))
+	for { // Infinite loop to read and write from clients (echoing messages back)
+		n, err := conn.Read((buf)) // Read from client
 		if err != nil {
 			fmt.Println("Error reading from client:", err)
 			return
 		}
 
-		_, err = conn.Write(buf[:n])
+		_, err = conn.Write(buf[:n]) // Write from client
 		if err != nil {
 			fmt.Println("Error writing to client:", err)
 			return
@@ -26,21 +26,21 @@ func handleConnection(conn net.Conn) {
 
 func main() {
 	port := ":4000"
-	listener, err := net.Listen("tcp", port)
+	listener, err := net.Listen("tcp", port) // Set up a tcp connection on port 4000
 
 	if err != nil {
 		panic(err)
 	}
 
-	defer listener.Close()
+	defer listener.Close() // Close connection at the end of the program
 
 	fmt.Printf("Server listening on %s\n", port)
-	for {
+	for { // Infinite loop to accept multiple connections
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println("Error accepting:", err)
 			continue
 		}
-		handleConnection(conn)
+		handleConnection(conn) // Function to handle connections
 	}
 }
